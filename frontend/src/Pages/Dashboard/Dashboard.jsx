fe2decc7bd575fe20396ece4fcacf1654ec85a1d
Millyaa-11 <chalitaes@gmail.com>
2024-03-08 01:47:47 +0700

    dashboard wait for integrate

--- frontend/src/Pages/Dashboard/Dashboard.jsx
+++ frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import './Dashboard.css';
+import topdec1 from '../../img/topdec1.png';
+import mrfresh from '../../img/mrfresh.png';
 
 const Dashboard = () => {
   return (
-    <div className="dashboard-container">
+    <div className='body'>
+      <img 
+      className='topdec'
+      src={topdec1}
+      alt="topdec"
+      />
+      <img 
+      className='mrfresh'
+      src={mrfresh}
+      alt="mrfresh"
+      />
       <div className="dashboard">
-        <h1>Welcome to Your Dashboard</h1>
-        <div className="widgets">
-          <div className="widget">
-            <h2>Widget 1</h2>
-            <p>Content for Widget 1</p>
+        <div className="graph">
+          <h1>ML to Time</h1>
+          <div className="widgets">
+            <div className="widget">
+              <h2>Graph</h2>
+              <p>Content for Widget 1</p>
+            </div>
           </div>
-          <div className="widget">
-            <h2>Widget 2</h2>
-            <p>Content for Widget 2</p>
+        </div>
+        <p className="heading">Personal Goals</p>
+        <div className="widget-container">
+          <div className="widget goal">
+            <h2>Goals</h2>
+            <p><strong>2000 ml</strong> ml Per Day</p>
+            <p><strong>9</strong> Drink Freq</p>
           </div>
-          <div className="widget">
-            <h2>Widget 3</h2>
-            <p>Content for Widget 3</p>
+          <div className="widget current">
+            <h2>Current</h2>
+            <p><strong>1000 ml</strong> ml Per Day</p>
+            <p><strong>4</strong> Drink Freq</p>
           </div>
         </div>
       </div>
