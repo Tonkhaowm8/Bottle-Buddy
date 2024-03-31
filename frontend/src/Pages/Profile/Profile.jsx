e50fba7d03849508c51405cce9e857bff3150810
Millyaa-11 <chalitaes@gmail.com>
2024-03-31 19:27:09 +0700

    fix logout still have bug

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -12,22 +12,42 @@ import { Link } from 'react-router-dom'; // Import Link from React Router
 const Profile = () => {
   const [userID, setUserID] = useState(null); // State to hold userID
 
-  // Logout handler function
   const handleLogout = () => {
-    // Your logout logic here
+    // Replace this with your actual client ID
+    const clientId = '7o199gv9aniv573gaa1fofb30h'; 
+    const logoutUri = encodeURIComponent('http://localhost:3000/signout');
+
+    const logoutUrl = `https://hydrobuduser.auth.ap-southeast-1.amazoncognito.com/logout?client_id=${clientId}&logout_uri=${logoutUri}`;
+
+    // Redirecting the user to the logout URL
+    window.location.href = logoutUrl;
+    console.log("signed out");
+  };
+
+  // Function to check if cookie exists
+  const checkCookie = () => {
+    return document.cookie !== '';
+  };
+
+  // Function to redirect to Cognito login
+  const redirectToLogin = () => {
+    const loginUrl = `https://hydrobuduser.auth.ap-southeast-1.amazoncognito.com/oauth2/authorize?client_id=7o199gv9aniv573gaa1fofb30h&response_type=token&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fmaster.d2gzog98ma463h.amplifyapp.com%2Fsignin%2F`;
+    window.location.href = loginUrl;
   };
 
   // Check for and extract user information from id_token on component mount
   useEffect(() => {
-    const extractAndDecodeIdToken = () => {
+    const cookieExists = checkCookie(); 
+    if (!cookieExists) {
+      redirectToLogin();
+    } else {
       const cookies = document.cookie.split(';');
       let idToken = null;
 
-      // Iterate through each cookie to find the one containing the id_token
       cookies.forEach(cookie => {
         const [name, value] = cookie.trim().split('=');
-        if (name === 'id_token') { // Assuming 'id_token' is the name of the cookie
-          idToken = value; // Assign the token value to idToken
+        if (name === 'id_token') {
+          idToken = value;
         }
       });
 
@@ -35,11 +55,9 @@ const Profile = () => {
         try {
           const tokenParts = idToken.split('.');
           if (tokenParts.length === 3) {
-            const decodedToken = JSON.parse(window.atob(tokenParts[1])); // Decode the payload and parse as JSON
-
-            // Extracting the username from the decoded token
+            const decodedToken = JSON.parse(window.atob(tokenParts[1]));
             const userIDFromToken = decodedToken ? decodedToken['cognito:username'] : null;
-            setUserID(userIDFromToken); // Set userID state with the username from Cognito
+            setUserID(userIDFromToken);
           } else {
             console.error('Invalid JWT format:', idToken);
           }
@@ -49,15 +67,11 @@ const Profile = () => {
       } else {
         console.error('id_token not found in cookies');
       }
-    };
+    }
+  }, []);
 
-    extractAndDecodeIdToken();
-  }, []); // Empty dependency array to run only once on mount
-
-  // Handle sound on
   const handleSoundOn = async () => {
-    const data = { "sound": "On" }; // Assuming mlPerDay is defined somewhere
-
+    const data = { "sound": "On" };
     try {
       const response = await fetch('https://tr7zuzh1o1.execute-api.ap-southeast-1.amazonaws.com/sound', {
         method: 'POST',
@@ -74,14 +88,11 @@ const Profile = () => {
       console.log('Sound turned on successfully!');
     } catch (error) {
       console.error('Error turning sound on:', error);
-      // Handle error appropriately (e.g., show error message to the user)
     }
   };
 
-  // Handle sound off
   const handleSoundOff = async () => {
-    const data = { "sound": "Off" }; // Assuming mlPerDay is defined somewhere
-
+    const data = { "sound": "Off" };
     try {
       const response = await fetch('https://tr7zuzh1o1.execute-api.ap-southeast-1.amazonaws.com/sound', {
         method: 'POST',
@@ -98,10 +109,8 @@ const Profile = () => {
       console.log('Sound turned off successfully!');
     } catch (error) {
       console.error('Error turning sound off:', error);
-      // Handle error appropriately (e.g., show error message to the user)
     }
   };
-
 
   return (
     <div className='body'>
@@ -119,12 +128,11 @@ const Profile = () => {
           />
         </Link>
       </div>
-      {/* Logout button */}
       <img 
         className='logout'
         src={logoutIcon}
         alt="logout"
-        onClick={handleLogout} // Attach the logout handler to the onClick event
+        onClick={handleLogout}
       />
       <div className="profilecontainer">
         <img 
@@ -132,7 +140,7 @@ const Profile = () => {
           src={mrfreshprof}
           alt="mrfreshprof"
         />
-        <p className="mrFreshText"><strong>{userID}</strong></p> {/* Display userID */}
+        <p className="mrFreshText"><strong>{userID}</strong></p>
       </div>
       <img 
         className='preference'
@@ -140,18 +148,18 @@ const Profile = () => {
         alt="preference"
       />
       <div className= 'speakercontain'>
-      <img 
-        className='speaker_on'
-        src={speaker_on}
-        alt="speaker_on"
-        onClick={handleSoundOn}
-      />
-      <img 
-        className='speaker_off'
-        src={speaker_off}
-        alt="speaker_off"
-        onClick={handleSoundOff}
-      />
+        <img 
+          className='speaker_on'
+          src={speaker_on}
+          alt="speaker_on"
+          onClick={handleSoundOn}
+        />
+        <img 
+          className='speaker_off'
+          src={speaker_off}
+          alt="speaker_off"
+          onClick={handleSoundOff}
+        />
       </div>
     </div>
   );
